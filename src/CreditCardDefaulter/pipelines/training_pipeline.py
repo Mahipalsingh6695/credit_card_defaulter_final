d9a127ae75a0d7ea53282479b9daa8020a31c98f
Mahipal Singh <mahipalsinghchauhan1995@gmail.com>
2024-02-19 19:46:14 +0530

    updated

--- src/CreditCardDefaulter/pipelines/training_pipeline.py
+++ src/CreditCardDefaulter/pipelines/training_pipeline.py
@@ -17,6 +17,8 @@ train_data_path,test_data_path=obj.initiate_data_ingestion()
 
 data_transformation=DataTransformation()
 
+
+
 train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
 
 
